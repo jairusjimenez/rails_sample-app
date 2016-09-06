f6a26d4bcb0e6fd31ec64bad424d9e9148c9c0cb
jairusjimenez <jairusjimenez@live.com>
2016-09-07 01:46:50 +0800

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,18 @@ User.create!(name:  "Jairus Jimenez",
 			             email: "jairusjimenez@live.com",
 			             password:              "QWEASD21",
 			             password_confirmation: "QWEASD21",
-			             admin: true)
+			             admin: true,
+			             activated: true,
+			             activated_at: Time.zone.now)
 
 99.times do |n|
 	name = Faker::Name.name
 	email = "example-#{n-1}@railstutorial.org"
 	password = "password"
-	User.create!(name: name, email: email, password: password, password_confirmation: password)
+	User.create!(name: name, 
+		email: email, 
+		password: password, 
+		password_confirmation: password,
+		activated: true,
+		activated_at: Time.zone.now)
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -73,7 +73,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated? shoul return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 end
 
